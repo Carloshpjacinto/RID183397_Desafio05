9ebe539f18ffca65ab62801d6860325fda51a628
Carloshpjacinto <carlo.henrique37@gmail.com>
2024-12-10 14:44:46 -0300

    feature/testando-conexao-front-end

--- src/views/LivrosCadastro/LivrosCadastro.jsx
+++ src/views/LivrosCadastro/LivrosCadastro.jsx
@@ -8,25 +8,38 @@ const LivrosCadastro = () => {
   
   const [livro, setLivro] = useState([])
 
-  async function createLivro(){
+  async function createLivro(event){
+
+    event.preventDefault();
+
+    const { id, titulo, num_paginas, isbn, editora } = livro;
+
     const body = {
-        id:Number(livro.id),
-        titulo:livro.titulo,
-        num_paginas: Number(livro.num_paginas),
-        isbn: livro.isbn,
-        editora: livro.editora
-      }
-      if(livro.id!=undefined && livro.id!='' && livro.titulo!=undefined && livro.titulo!='' && livro.num_paginas!=undefined && livro.num_paginas!='' && livro.isbn !=undefined && livro.isbn !='' && livro.editora !=undefined && livro.editora !=''){
-      await LivrosService.createLivro(body)
-      .then((response)=>{
-        alert(response.data)
-        document.getElementById('formulario').reset
-      })
-      .catch(({response:{data,status}})=>{
-        alert(`${status} - ${data}`)      
-      });
+
+      id:Number(id),
+      titulo,
+      num_paginas: Number(num_paginas),
+      isbn,
+      editora
     }
 
+    if(!id || !titulo || !num_paginas || !isbn || !editora){
+
+      alert("Todos os campos são Obrigatorios");
+      return;
+    }
+
+    try{
+
+      const response =  await LivrosService.createLivro(body);
+      alert("Livro cadastrado com sucesso!")
+      document.getElementById("formulario").reset();
+      setLivro({ id: '', titulo: '', num_paginas: '', isbn: '', editora: '' });
+
+    }catch(err){
+
+      alert("Erro ao cadastrar livro")
+    }
   }
 
   return (
@@ -36,31 +49,29 @@ const LivrosCadastro = () => {
     <div className='livrosCadastro'>
         <h1>Cadastro de Livros</h1>
         <div>          
-          <form id="formulario">
+          <form id="formulario" onSubmit={createLivro}>
           <div className='form-group'>
             <label>Id</label>
-            <input type="text" id='id' required onChange={(event)=>{ setLivro({...livro, id: event.target.value})}} ></input>
+            <input type="text" id='id' value={livro.id} required onChange={(event)=>{ setLivro({...livro, id: event.target.value})}} ></input>
           </div>
           <div className='form-group'>
             <label>Titulo</label>
-            <input type="text" id='titulo' required onChange={(event)=>{ setLivro({...livro, titulo: event.target.value})}}></input>
+            <input type="text" id='titulo' value={livro.titulo} required onChange={(event)=>{ setLivro({...livro, titulo: event.target.value})}}></input>
           </div>
           <div className='form-group'>
             <label>Número de Páginas</label>
-            <input type="text" id='num' required onChange={(event)=>{ setLivro({...livro, num_paginas: event.target.value})}}></input>
+            <input type="text" id='num' value={livro.num_paginas} required onChange={(event)=>{ setLivro({...livro, num_paginas: event.target.value})}}></input>
           </div>
           <div className='form-group'>
             <label>ISBN</label>
-            <input type="text" id='isbn' required onChange={(event)=>{ setLivro({...livro, isbn: event.target.value})}}></input>
+            <input type="text" id='isbn' value={livro.isbn} required onChange={(event)=>{ setLivro({...livro, isbn: event.target.value})}}></input>
           </div>
           <div className='form-group'>
             <label>Editora</label>
-            <input type="text" id='editora' required onChange={(event)=>{ setLivro({...livro, editora: event.target.value})}}></input>
+            <input type="text" id='editora' value={livro.editora} required onChange={(event)=>{ setLivro({...livro, editora: event.target.value})}}></input>
           </div> 
           <div className='form-group'>
-            <button onClick={()=>{
-              createLivro()
-            }}>Cadastrar Livro</button>  
+            <button type='submit'>Cadastrar Livro</button>  
           </div>         
           </form>
         </div>
